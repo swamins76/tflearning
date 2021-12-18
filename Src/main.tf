d03583602a7e103d149fcdd883bad982340894b4
Swaminathan Shankar Subbu <swaminathans76@gmail.com>
2021-12-18 18:27:19 -0500

    first draft

---
+++ Src/main.tf
@@ -0,0 +1,10 @@
+#Create a web server through bootstrap method
+resource "aws_instance" "tfwebserver" {
+    ami = "ami-0ed9277fb7eb570c9"
+    instance_type = "t2.micro"
+    tags = {
+        Name = "TFcreated"
+        env = "Test"
+    }
+  
+}
