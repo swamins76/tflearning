91710824e18364d9d3c5ea0cb915e89abe8a77eb
Swaminathan Shankar Subbu <swaminathans76@gmail.com>
2021-12-20 18:55:34 -0500

    updated

--- Src/main.tf
+++ Src/main.tf
@@ -1,21 +1,11 @@
 #Create a web server through bootstrap method
-resource "aws_instance" "tfwebserver" {
-    ami = "ami-0ed9277fb7eb570c9"
-    instance_type = "t2.micro"
-    tags = {
-        Name = "TFcreated"
-        env = "Prod"
-    }
-  
-}
-
 resource "aws_instance" "tfwebserverwest" {
     provider = aws.west
     ami = "ami-03af6a70ccd8cb578"
     instance_type = "t2.micro"
     tags = {
         Name = "TFcreated"
-        env = "Prod"
+        env = "${terraform.workspace}"
     }
   
 }
---
+++ training/src/provider.tf
@@ -0,0 +1,10 @@
+provider "aws" {
+    region = "us-east-1"
+  
+}
+
+provider "aws" {
+   alias = "west" 
+    region = "us-west-1"
+  
+}
