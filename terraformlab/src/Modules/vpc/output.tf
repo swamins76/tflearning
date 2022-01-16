fc9f397993076817d80778d3511501e25a18399f
Swaminathan S <swaminathans76@gmail.com>
2022-01-16 18:20:42 -0500

    updated with more network components

--- terraformlab/src/Modules/vpc/output.tf
+++ terraformlab/src/Modules/vpc/output.tf
@@ -1,16 +1,27 @@
-output "subnet_id_web1" {
-  value = aws_subnet.web1.id
+ output "master_vpc_id" {
+  value = aws_vpc.tf-vpc-master.id
+
+} 
+
+ output "worker_vpc_id" {
+  value = aws_vpc.tf-vpc-worker.id
+
+} 
+
+output "master_subnet_id_1" {
+  value = aws_subnet.master-subnet-1.id
+} 
+
+output "master_subnet_id_2" {
+  value = aws_subnet.master-subnet-2.id
+} 
+
+output "worker_subnet_id_1" {
+  value = aws_subnet.worker-subnet-1.id
+} 
+
+output "route_table_assoc_master"{
+  value = aws_main_route_table_association.set-master-default-rt-assoc
 }
 
-/* output "subnet_id_web2" {
-  value = aws_subnet.web2.id
-} */
 
-output "awsvpcid" {
-
-  value = aws_vpc.tfvpc.id
-}
-
-output "awsvpc" {
-  value = aws_vpc.tfvpc
-}
